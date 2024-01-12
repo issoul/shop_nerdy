537056d338b26436df44f635f7e8b232968387ee
issoul <153714324+issoul@users.noreply.github.com>
2024-01-12 18:05:41 +0900

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,46 @@
+
+// 헤더 메뉴 드롭다운 //
+const ddup = document.querySelectorAll('.drop_up');
+const ddown = document.querySelectorAll('.drop_down');
+
+
+
+    ddup[0].addEventListener('mouseover', ()=> {
+        ddown[0].classList.add('open');
+    })
+    ddup[0].addEventListener('mouseout', ()=> {
+        ddown[0].classList.remove('open');
+    })
+
+    ddup[1].addEventListener('mouseover', ()=> {
+        ddown[1].classList.add('open');
+    })
+    ddup[1].addEventListener('mouseout', ()=> {
+        ddown[1].classList.remove('open');
+    })
+
+    ddup[2].addEventListener('mouseover', ()=> {
+        ddown[2].classList.add('open');
+    })
+    ddup[2].addEventListener('mouseout', ()=> {
+        ddown[2].classList.remove('open');
+    })
+
+
+    ddup[3].addEventListener('mouseover', ()=> {
+        ddown[3].classList.add('open');
+    })
+    ddup[3].addEventListener('mouseout', ()=> {
+        ddown[3].classList.remove('open');
+    })
+
+
+    ddup[4].addEventListener('mouseover', ()=> {
+        ddown[4].classList.add('open');
+    })
+    ddup[4].addEventListener('mouseout', ()=> {
+        ddown[4].classList.remove('open');
+    })
+
+
+ 
