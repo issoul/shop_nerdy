d43d56c92aaa9800a541680a6f3b86323f12794f
issoul <153714324+issoul@users.noreply.github.com>
2024-01-15 18:02:08 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,49 +1,49 @@
 
-// 헤더 메뉴 드롭다운 //
+// // 헤더 메뉴 드롭다운 //
 
-const ddup = document.querySelectorAll('.drop_up');
-const ddown = document.querySelectorAll('.drop_body');
-
-
-    ddup[0].addEventListener('mouseover', ()=> {
-        ddown[0].classList.add('open');
-        ddown[0].style.height='361px';
-    })
-    ddup[0].addEventListener('mouseout', ()=> {
-        ddown[0].classList.remove('open');
-    })
-
-    ddup[1].addEventListener('mouseover', ()=> {
-        ddown[1].classList.add('open');
-        ddown[1].style.height='242px';
-    })
-    ddup[1].addEventListener('mouseout', ()=> {
-        ddown[1].classList.remove('open');
-    })
-
-    ddup[2].addEventListener('mouseover', ()=> {
-        ddown[2].classList.add('open');
-        ddown[2].style.height='58px';
-    })
-    ddup[2].addEventListener('mouseout', ()=> {
-        ddown[2].classList.remove('open');
-    })
+// const ddup = document.querySelectorAll('.drop_up');
+// const ddown = document.querySelectorAll('.drop_body');
 
 
-    ddup[3].addEventListener('mouseover', ()=> {
-        ddown[3].classList.add('open');
-        ddown[3].style.height='58px';
-    })
-    ddup[3].addEventListener('mouseout', ()=> {
-        ddown[3].classList.remove('open');
-    })
+//     ddup[0].addEventListener('mouseover', ()=> {
+//         ddown[0].classList.add('open');
+//     })
+//     ddup[0].addEventListener('mouseout', ()=> {
+//         ddown[0].classList.remove('open');
+//         ddown[0].style.height='361px';
+//     })
+
+//     ddup[1].addEventListener('mouseover', ()=> {
+//         ddown[1].classList.add('open');
+//     })
+//     ddup[1].addEventListener('mouseout', ()=> {
+//         ddown[1].classList.remove('open');
+//         ddown[1].style.height='242px';
+//     })
+
+//     ddup[2].addEventListener('mouseover', ()=> {
+//         ddown[2].classList.add('open');
+//     })
+//     ddup[2].addEventListener('mouseout', ()=> {
+//         ddown[2].classList.remove('open');
+//         ddown[2].style.height='58px';
+//     })
 
 
-    ddup[4].addEventListener('mouseover', ()=> {
-        ddown[4].classList.add('open');
-        ddown[4].style.height='58px';
-    })
-    ddup[4].addEventListener('mouseout', ()=> {
-        ddown[4].classList.remove('open');
-    })
+//     ddup[3].addEventListener('mouseover', ()=> {
+//         ddown[3].classList.add('open');
+//     })
+//     ddup[3].addEventListener('mouseout', ()=> {
+//         ddown[3].classList.remove('open');
+//         ddown[3].style.height='58px';
+//     })
+
+
+//     ddup[4].addEventListener('mouseover', ()=> {
+//         ddown[4].classList.add('open');
+//     })
+//     ddup[4].addEventListener('mouseout', ()=> {
+//         ddown[4].classList.remove('open');
+//         ddown[4].style.height='58px';
+//     })
 
